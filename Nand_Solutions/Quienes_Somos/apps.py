aa81e1742db71833db2d74930702c44a3a05f4b4
WhenBut18 <juanpablo_fuenzalida08@hotmail.com>
2023-09-26 21:31:05 -0300

    Restart

--- Nand_Solutions/Quienes_Somos/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class QuienesSomosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Quienes_Somos'
--- Secret_Proyect/mysite/urls.py
+++ Secret_Proyect/mysite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Nand_Solutions project.
+URL configuration for mysite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,16 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Quienes_Somos'))
-]
-
-from django.urls import path
-from Quienes_Somos import views
-
-urlpatterns = [
-    path('', views.index),
 ]
